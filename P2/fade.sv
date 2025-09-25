d444e45fee7a3c3007c6348a4f4e8b105d3b7165
xavieryn <xavier.nishikawa@gmail.com>
2025-09-25 17:05:05 -0400

    Moves code out of examples

--- P2/fade.sv
+++ P2/fade.sv
@@ -6,11 +6,11 @@
 // 'posedge clk' refers to the positive edge of a clock signal
 module fade #(
      // CLK frequency is 12MHz, (the whole loop should be a total of 1 second)
-    parameter INC_DEC_INTERVAL = 20000, // 1/6 of a second (Add an extra 0 to see if there is physical difference (20,000,000))
+    parameter INC_DEC_INTERVAL = 200000, 
+    // 1/6 of a second (Add an extra 0 to see if there is physical difference (20,000,000))
     // Realizing a bug in code is that the inc_dec_max is what actually affects the pwm (how strong the light is)
     // which means i need to probably add anoother variable that counts once the inc_dec_max gets big enough,
     // then it will add to another variable that will change the color
-
     parameter PWM_ROUNDS = 100, // 20,000 * 100 * 6 = 12,000,000
     parameter INC_DEC_MAX = 6, // Transition to 6 different stages which happens every 1/6 of a second
     parameter PWM_INTERVAL = 1200, // CLK frequency is 12MHz, so 1,200 cycles is 100us
@@ -105,40 +105,24 @@ module fade #(
      // HSV Color Wheel Implementation
     always_ff @(posedge time_to_inc_dec) begin
         case(inc_dec_count)
-            0: begin // RED → YELLOW (increase green)
+            0: // RED → YELLOW (increase green)
                 // R=max, G: 0→max, B=0
                    pwm_valueG <= pwm_valueG + INC_DEC_VAL;
-            end
-            
-            1: begin // YELLOW → GREEN (decrease red)
+            1: // YELLOW → GREEN (decrease red)
                 // R: max→0, G=max, B=0  
                     pwm_valueR <= pwm_valueR - INC_DEC_VAL;
-            
-            end
-            
-            2: begin // GREEN → CYAN (increase blue)
+            2: // GREEN → CYAN (increase blue)
                 // R=0, G=max, B: 0→max
                     pwm_valueB <= pwm_valueB + INC_DEC_VAL;
-       
-            end
-            
-            3: begin // CYAN → BLUE (decrease green)
+            3: // CYAN → BLUE (decrease green)
                 // R=0, G: max→0, B=max
-                    pwm_valueG <= pwm_valueG - INC_DEC_VAL;
-              
-            end
-            
-            4: begin // BLUE → MAGENTA (increase red)
+                    pwm_valueG <= pwm_valueG - INC_DEC_VAL;         
+            4: // BLUE → MAGENTA (increase red)
                 // R: 0→max, G=0, B=max
-                    pwm_valueR <= pwm_valueR + INC_DEC_VAL;
-             
-            end
-            
-            5: begin // MAGENTA → RED (decrease blue)
+                    pwm_valueR <= pwm_valueR + INC_DEC_VAL;             
+            5: // MAGENTA → RED (decrease blue)
                 // R=max, G=0, B: max→0
                     pwm_valueB <= pwm_valueB - INC_DEC_VAL;
-                
-            end
         endcase
     end
 endmodule
