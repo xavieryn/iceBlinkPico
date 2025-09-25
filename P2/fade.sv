e2671de6bb2b7241a71bed9ef358625bc77b3944
xavieryn <xavier.nishikawa@gmail.com>
2025-09-25 17:29:47 -0400

    Adds comments to make code comprehensible

--- P2/fade.sv
+++ P2/fade.sv
@@ -6,13 +6,9 @@
 // 'posedge clk' refers to the positive edge of a clock signal
 module fade #(
      // CLK frequency is 12MHz, (the whole loop should be a total of 1 second)
-    parameter INC_DEC_INTERVAL = 200000, 
-    // 1/6 of a second (Add an extra 0 to see if there is physical difference (20,000,000))
-    // Realizing a bug in code is that the inc_dec_max is what actually affects the pwm (how strong the light is)
-    // which means i need to probably add anoother variable that counts once the inc_dec_max gets big enough,
-    // then it will add to another variable that will change the color
-    parameter PWM_ROUNDS = 100, // 20,000 * 100 * 6 = 12,000,000
-    parameter INC_DEC_MAX = 6, // Transition to 6 different stages which happens every 1/6 of a second
+    parameter INC_DEC_INTERVAL = 20000, // Add an extra 0 if you want to visually see the change (200,000)
+    parameter PWM_ROUNDS = 100, // 20,000 * 100 * 6 = 12,000,000 (12,000,000 represents 1 second, so we make sure we go through the 360 degrees in 1 second)
+    parameter DEGREE_STAGE = 6, // Transition to 6 different stages which happens every 1/6 of a second
     parameter PWM_INTERVAL = 1200, // CLK frequency is 12MHz, so 1,200 cycles is 100us
     parameter INC_DEC_VAL = PWM_INTERVAL / (PWM_ROUNDS),
     parameter CLKFREQ = 1200000
@@ -32,21 +28,21 @@ module fade #(
     logic next_state;
 
     // Declare variables for timing state transitions
-    logic [$clog2(INC_DEC_INTERVAL) - 1:0] count = 0;
-    logic [$clog2(INC_DEC_MAX) - 1:0] inc_dec_count = 0;
-    logic [$clog2(PWM_ROUNDS) - 1:0] pwm_counter = 0;
-    logic time_to_inc_dec = 1'b0;
-    logic time_to_transition = 1'b0;
+    logic [$clog2(INC_DEC_INTERVAL) - 1:0] count = 0; // when to switch pwm state (adding/subtracting pwm_value)
+    logic [$clog2(PWM_ROUNDS) - 1:0] pwm_count = 0; // how many times pwm state has been increased
+    logic [$clog2(DEGREE_STAGE) - 1:0] degree_count = 0; // what stage/degree program is on
+    logic time_to_inc_dec = 1'b0; 
+    logic time_to_transition = 1'b0; 
     logic time_to_go = 1'b0;
 
-    // start value (nothing should be on because the pwm is basically saying 0% duty)
+    // start values
     initial begin
-        pwm_valueR = PWM_INTERVAL; // it seems like my r is affecting my green and am not sure why
+        pwm_valueR = PWM_INTERVAL; // setting this to maximum because red is 0 degreess
         pwm_valueG = 0;
         pwm_valueB = 0;
     end
 
-    // Register the next state of the FSM
+    // Register the next state of the FSM (triggers every 1/6 of a second)
     always_ff @(posedge time_to_go) // waiting for time_transition to go from 0 to 1
         current_state <= next_state;
 
@@ -65,7 +61,7 @@ module fade #(
     // Stands for always flip flop (moving syncronyously with a clock) 
     // Means this function is getting checked every time
     always_ff @(posedge clk) begin // changes every 
-        if (count == INC_DEC_INTERVAL - 1) begin // if it equals 2000000 (changing every 1/6 of a second)
+        if (count == INC_DEC_INTERVAL - 1) begin // if it equals 20000 (changing every 1/600 of a second)
             count <= 0; // reset
             time_to_inc_dec <= 1'b1; // turn positive
         end
@@ -76,35 +72,33 @@ module fade #(
     end
 
     always_ff @(posedge time_to_inc_dec) begin
-        if (pwm_counter == PWM_ROUNDS - 1) begin // if it goes through all 6 counts (mode), then it resets back to the start
-            pwm_counter <= 0;
+        if (pwm_count == PWM_ROUNDS - 1) begin // if it goes through all 100 pwm intervals 0 - 1200 (in increments of 12 because 1200/100 = 12)
+            pwm_count <= 0;
             time_to_transition <= 1'b1;
         end
-
         else begin
-            pwm_counter <= pwm_counter + 1;
+            pwm_count <= pwm_count + 1;
             time_to_transition <= 1'b0;
         end
     end
 
-
+    // changes every 1/6 of a second so it switches to next mode (next 60 degrees)
     always_ff @(posedge time_to_transition) begin
-        if (inc_dec_count == INC_DEC_MAX - 1) begin // if it goes through all 6 counts (mode), then it resets back to the start
-            inc_dec_count <= 0;
+        if (degree_count == DEGREE_STAGE - 1) begin // if it goes through all 6 counts (mode), then it resets back to the start
+            degree_count <= 0;
             time_to_go <= 1'b1;
         end
-
         else begin
-            inc_dec_count <= inc_dec_count + 1;
+            degree_count <= degree_count + 1;
             time_to_go <= 1'b0;
         end
     end
 
+    // HSV Color Wheel Implementation
     // Implement counter for timing state transitions
-    // 1/6 of a second (values only change when time_to_inc_dec changes)
-     // HSV Color Wheel Implementation
+    // 1/600 of a second (values only change when time_to_inc_dec changes)
     always_ff @(posedge time_to_inc_dec) begin
-        case(inc_dec_count)
+        case(degree_count)
             0: // RED → YELLOW (increase green)
                 // R=max, G: 0→max, B=0
                    pwm_valueG <= pwm_valueG + INC_DEC_VAL;
