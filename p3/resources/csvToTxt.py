46f6d57edfb92fe4110b52696016a267f520428a
xavieryn <xavier.nishikawa@gmail.com>
2025-10-20 12:17:55 -0400

    forgot that pandas considers first row headers

--- p3/resources/csvToTxt.py
+++ p3/resources/csvToTxt.py
@@ -3,14 +3,14 @@ import numpy as np
 
 filepath = "/Applications/oss-cad-suite/iceBlinkPico/p3/resources/"
 filename = input("csv pls: ")
-df = pd.read_csv(filepath+filename+'.csv')
+df = pd.read_csv(filepath+filename+'.csv', header=None)
 
 textfile = filename + ".txt"
 
 dfarray = df.to_numpy()
 
 print(dfarray)
-
+print(len(dfarray), ' + ', len(dfarray[0]))
 with open(filepath+textfile, 'w') as file:
     for rows in range (len(dfarray)):
         for col in range(len(dfarray[0])):
