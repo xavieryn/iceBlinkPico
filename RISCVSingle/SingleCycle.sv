2240096b973c16b43eba42c6d865b98383d02daf
xavieryn <xavier.nishikawa@gmail.com>
2025-11-24 22:53:52 -0800

    Starting to add a bunch of comments to try to fully understand the code

--- RISCVSingle/SingleCycle.sv
+++ RISCVSingle/SingleCycle.sv
@@ -6,21 +6,24 @@ Then converted the code to System Verilog as the code was originally in Verilog
 */
 
 // Program Counter
+// where we are in the program
 module Program_counter(clk, reset, PC_in, PC_out);
 
     input clk, reset;
-    input  [31:0] PC_in;
-    output reg [31:0] PC_out;
+    input  [31:0] PC_in; // where we go next
+    output reg [31:0] PC_out; // where we are
 
     always_ff @(posedge clk or posedge reset)
     begin  
         if(reset)
-        PC_out <= 32'b00; // reset everything to 0. 
+        PC_out <= 32'b00; // reset back to address 0
         else 
-        PC_out <= PC_in;
+        PC_out <= PC_in; // go to next address 
     end
 endmodule
 
+// calculates the next sequential instruction address
+// ex. 0x04 -> +4 -> 0x08 (goes into mux2 which decides whether to use this for next pc or if it jumped (branch))
 module PCplus4(fromPC, NextoPC);
 
     input [31:0] fromPC;
@@ -31,7 +34,6 @@ module PCplus4(fromPC, NextoPC);
 endmodule
 
 // Instruction Memory 
-
 module Instruction_Mem(clk, reset, read_address, instruction_out);
 
 input clk, reset;
@@ -62,21 +64,20 @@ begin
     if(reset)
         instruction_out <= 32'h00000000;
     else 
-        instruction_out <= I_Mem[read_address];
+        instruction_out <= I_Mem[read_address]; // reads instruction in address (32 bits / 4 bytes)
 end
 
 endmodule
 
 //Register File
-
 module Reg_File(clk, reset, RegWrite, Rs1, Rs2, Rd, Write_data, read_data1, read_data2);
 
-input clk, reset, RegWrite;
+input clk, reset, RegWrite; // regwrite is yes or no
 input [4:0] Rs1, Rs2, Rd; 
 input [31:0] Write_data;
 int k;
 
-output [31:0] read_data1, read_data2;
+output [31:0] read_data1, read_data2; //
 
 reg [31:0] Registers[31:0];
 
@@ -85,21 +86,20 @@ begin
     if (reset)
     begin
         for(k= 0; k<32; k=k+1)begin
-            Registers[k] <= 32'b00;
+            Registers[k] <= 32'b00; // set every register to 0 
         end
     end
-    else if (RegWrite && Rd != 0) begin
-        Registers[Rd] <= Write_data; // write to register destination
+    else if (RegWrite && Rd != 0) begin // dont write to the 0th register 
+        Registers[Rd] <= Write_data; // write to register destination (comes from WriteBack_top, happens right at the clock edge, so everything was computed already, and waiting to get assigned)
     end
 end
 
-assign read_data1 = Registers[Rs1];
-assign read_data2 = Registers[Rs2];
-
+// all happen immediately because it is combinational (always active as well)
+assign read_data1 = Registers[Rs1]; // reads register1 address
+assign read_data2 = Registers[Rs2]; // reads register2 address 
 endmodule
 
-
-// Immediate Generator 
+// Immediate Generator (what to do if its an immediate type)
 module ImmGen(Opcode, instruction, ImmExt);
 
 input [6:0] Opcode;
@@ -114,52 +114,57 @@ begin
     // Sign extending is for the ALU because it expects 32 bits, but for immediate, we only use the first 12 bits
     // I-type ALU (ADDI, etc.) 
     7'b0010011 : ImmExt = {{20{instruction[31]}}, instruction[31:20]};
-    // S-type
-    7'b0100011 : ImmExt = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+    // S-type (Store)
+    7'b0100011 : ImmExt = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]}; 
+    // 31:25 are the 7 upper immediate bits in s-type and 11:7 are the 5 lower  immediate bits. They are just split like that to make
+    // register locations consistent 
     // first 20 bits are sign extended, and the two other are put together (making a 12 bit immediate like above)
-    // B-type
-    7'b1100011 : ImmExt = {{19{instruction[31]}}, instruction[31], instruction[30:25], instruction[11:8], 1'b0};
+    // B-type (Branch)
+    // CHECK THIS... THIS MIGHT BE WRONG
+    7'b1100011 : ImmExt = {{19{instruction[31]}}, instruction[31], instruction[30:25], instruction[11:8], 1'b0}; // why is this only 31 like Jack said
     // first 19 bits are sign extended, and the two other are put together (making a 12 bit immediate like above)
     default: ImmExt = 32'b0;
     endcase
 end
 endmodule
 
-// Control Unit (MAIN DECODER) generates the high-level control signals for the data path
+
+// Control Unit (MAIN DECODER) generates the high-level control signals for the data path (opcode translator)
+// LOOKING AT OPCODE
 // branch is for stuff like BEQ
 // MemRead is for instructions like lw
-// ALUOp // is for what type of R-type, I-tpe instruction
+// ALUOp // is for what type of R-type, I-type instruction
 // Memwrite is for sw (store word) 
-//
 module Control_Unit(instruction, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite);
 
 input[6:0] instruction;
 
-output reg Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite; // which in struction to actually run
+output reg Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite; // which instruction to actually run
 output reg [1:0] ALUOp;
 
+// 8 bits because alu op is 2 bits (like you can see up top)
 always_comb
 begin
     case(instruction)
-    // R-type instruction
-    7'b0110011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b00100010; // R-type
+    // R-type instruction (add, compare, all that vibe) 
+    7'b0110011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b00100010; 
     // I-type Load
-    7'b0000011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b11110000; // Load
-    // I-type ALU (ADDI) - YES YOU NEED THIS!
-    7'b0010011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b10100000; // I-type ALU
+    7'b0000011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b11110000;
+    // I-type ALU (ADDI)
+    7'b0010011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b10100000;
     // S-type Store
-    7'b0100011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b10001000; // Store
+    7'b0100011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b10001000;
     // B-type Branch
-    7'b1100011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b00000101; // Branch
+    7'b1100011 : {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b00000101;
     default:     {ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp} = 8'b00000000;
     endcase
 end
 endmodule
 
-// ALU 
+
+// ALU () 
 module ALU_unit(A, B, Control_in, ALU_Result, zero);
     input [31:0] A, B;
-
     input [3:0] Control_in;
     output reg zero; 
     output reg[31:0] ALU_Result; 
@@ -167,10 +172,10 @@ module ALU_unit(A, B, Control_in, ALU_Result, zero);
     always_comb
     begin
         case(Control_in)
-        4'b0000: begin zero = 0; ALU_Result = A & B; end
-        4'b0001: begin zero = 0; ALU_Result = A | B; end 
-        4'b0010: begin zero = 0; ALU_Result = A + B; end
-        4'b0110: begin if (A==B) zero = 1; else zero = 0; ALU_Result = A - B; end
+        4'b0000: begin zero = 0; ALU_Result = A & B; end // and 
+        4'b0001: begin zero = 0; ALU_Result = A | B; end // or 
+        4'b0010: begin zero = 0; ALU_Result = A + B; end // adder 
+        4'b0110: begin if (A==B) zero = 1; else zero = 0; ALU_Result = A - B; end // subtract, finds if value is 0
         default: begin zero = 0; ALU_Result = 0; end  // default in case its cooked
         endcase
     end
@@ -238,8 +243,8 @@ endmodule
 
 
 module Mux2(sel2, A2, B2, Mux2out);
-input sel2;
-input [31:0] A2, B2; 
+input sel2; // which gets decided 
+input [31:0] A2, B2; // A2 (Next instruction pcnext4) B2 (if branch instruction was called, so jump to different address)
 output [31:0] Mux2out; 
 
 assign Mux2out = (sel2==1'b0) ? A2 : B2; 
